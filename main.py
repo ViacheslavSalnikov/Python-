288334cbb313fc5a08ee8d04b57ed19a9e18b506
Viacheslav <salnikov90@yandex.ru>
2024-01-10 23:59:15 +0500

    Python НАЧАЛО

---
+++ main.py
@@ -0,0 +1,77 @@
+# print('Введите первое число: 1')
+# a = int(input())
+
+# b = int(input('Введите второе число: '))2
+
+# print(a, '+', b, '=', a + b)
+
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# a = 5.8744534616
+# b = 4.555556658
+# print(round(a*b, 3))
+
+# a = 1 > 4     false
+# print(a)
+
+# a = 1 < 4 and 5 > 2     true
+# print(a)
+
+# a = 1 == 2   false
+# print(a)
+
+
+# a = 1 != 2     true
+# print(a)
+
+# a = 'cwq'      true
+# b = 'cwq'
+# print(a == b)
+
+# a = 1 < 3 < 3 < 10    false
+# print(a)
+
+
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же Маша')
+# elif username == 'Марина':
+#     print('Марина, мы тебя так ждали')
+# elif username == 'Ксюша':
+#     print('Ксюша, ТОП!!!!')
+# else:
+#     print('Привет, ', username)
+
+# i = 0
+# while i < 5:
+#     # if i == 3:
+#     #     break
+#     i = i + 1
+# else:
+#     print('Пожалуй')
+#     print('хватит )')
+# print(i)
+
+# a = 'qwerty'
+
+# for i in a:
+#     print(i)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for J in range(5):
+#         line += "*"
+#     print(line)
+
+# text = 'Съешь ещё ЭтиХ МяГКих ФранцузскиХ БулОчеК'
+# print(len(text)) подсчет символов
+# print('ещё' in text) true
+# print(text.lower())  опускает в нижний регистр
+# print(text.upper())   поднимает всё в верхний регистр
+# print(text.replace('ещё' , 'ЕЩЁ')) меняет сочетание символов в нашей строке 1 аргумент какой есть, 2 аргумент на какой поменять
