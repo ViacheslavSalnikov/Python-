abb465b8d0f7fcdeb8cfb5dd3fec7e9905ff928a
Viacheslav <salnikov90@yandex.ru>
2024-02-14 00:22:31 +0500

    good

--- main.py
+++ main.py
@@ -321,3 +321,50 @@
 #     return res
 
 # print(sum_str('q','e','l'))
+
+# def fib(n):
+#     if n in [1,2]:
+#         return 1
+#     return fib(n-1) + fib(n-2)
+# list_1 = []
+# for i in range(1, 10):
+#     list_1.append(fib(i))
+# print(list_1)
+
+# def quick_sort(array):
+#     if len(array) <= 1:
+#         return array
+#     else:
+#         pivot = array[0]                                        #Быстрая сортровка
+#     less = [i for i in array[1:] if i <= pivot]
+#     greater = [i for i in array[1:] if i > pivot]
+#     return quick_sort(less) + [pivot] + quick_sort(greater)
+# print(quick_sort([10, 5, 2]))
+
+def merge_sort(nums):
+    if len(nums) > 1:
+        mid = len(nums) // 2
+        left = nums[:mid]
+        right = nums[mid:]
+        merge_sort(left)
+        merge_sort(right)
+        i = j = k = 0
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                nums[k] = left[i]
+                i += 1
+            else:
+                nums[k] = right[j]
+                j +=1
+            k += 1
+        while i < len(left):
+            nums[k] = left[i]
+            i += 1
+            k +=1
+        while j < len(right):
+            nums[k] = right[j]
+            j += 1
+            k += 1
+list1 = [1, 5, 96, 67, 44, 145, 150, 105, 103]
+merge_sort(list1)
+print(list1)
---
+++ modulall.py
@@ -0,0 +1,9 @@
+# import modul1
+# print(modul1.max1(5, 9))
+
+# from modul1 import max1  # если поставить *, то будут импортироваться все функции из нужного нам модуля 
+
+# print(max1(5, 9))
+
+import modul1 as m1
+print(m1.max1(5,9))
